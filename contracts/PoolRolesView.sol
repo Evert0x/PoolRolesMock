ab1af714902b600779ecca9b955ce966ba40d991
Evert Kors <evert@hatching.io>
2021-01-23 14:03:51 +0100

    Add relayreceiver

--- contracts/PoolRolesView.sol
+++ contracts/PoolRolesView.sol
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pragma solidity ^0.7.4;
+import "./RelayReceiver.sol";
 
-contract PoolRolesView  {
+contract PoolRolesView is RelayReceiver {
     bytes32 internal constant DEFAULT_ADMIN_ROLE = 0x00;
     bytes32 internal constant MEMBER_ROLE = keccak256("MEMBER_ROLE");
     bytes32 internal constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
